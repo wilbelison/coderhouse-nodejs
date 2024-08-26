a72ed4bee84b8ca24486cebc0a12333916149fc3
Wilbelison Junior <meustudio@gmail.com>
2024-08-26 16:46:56 -0300

    css

--- src/app.js
+++ src/app.js
@@ -34,13 +34,13 @@ app.get("/", async (req, res) => {
         <link rel="stylesheet" href="./assets/styles/ProductManager.css">
       </head>
       <body>
-        <h1 class="stroke" data-content="Product Manager">Product Manager</h1>
+        <div class="title"><h1 class="stroke" data-content="Product Manager">Product Manager</h1></div>
         <ul class="options">
-          <li><a href="/products">➞ getProducts();<br /><span class="sub">Todos os produtos</span></a></li>
-          <li><a href="/products?limit=10">➞ getProducts(10);<br /><span class="sub">Limitado a 10 produtos</span></a></li>
-          <li><a href="/products/3">➞ getProductById(3);<br /><span class="sub">Produto com ID 3</span></a></li>
+          <li><a href="/products">getProducts();<br /><span class="sub">Todos os produtos</span></a></li>
+          <li><a href="/products?limit=10">getProducts(10);<br /><span class="sub">Limitado a 10 produtos</span></a></li>
+          <li><a href="/products/3">getProductById(3);<br /><span class="sub">Produto com ID 3</span></a></li>
         </ul>
-        <h2>Lista de produtos${limit ? ` limitado a ${limit}` : ""}</h2>
+        <h2 data-content="Lista de produtos${limit ? ` limitado a ${limit}` : ''}">Lista de produtos${limit ? ` limitado a ${limit}` : ''}</h2>
         <ul class="products-list">
           ${productsList}
         </ul>
@@ -61,7 +61,7 @@ app.get("/products", async (req, res) => {
 
 app.get("/products/:pid", async (req, res) => {
   const product = await Products.getProductById(parseInt(req.params.pid));
-  if (product) {
+  if (product) { 
     res.json(product);
   } else {
     res.status(404).json("Produto não encontrado");
