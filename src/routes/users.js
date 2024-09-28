9e1c5440af2f481ea8341d7c064c079edb85cac8
Wilbelison Junior <meustudio@gmail.com>
2024-09-28 10:10:04 -0300

    read multiple users and one user

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,26 +5,38 @@ const router = Router();
 let users = [];
 let id = 1;
 
-router.get("/", (req, res) => {
-  res.status(200).json(users);
+function isArray(obj) {
+  return !!obj && obj.constructor === Array;
+}
+
+router.get("/:id?", (req, res) => {
+  const id = req.params.id;
+  if (!req.params.id) {
+    res.status(200).json(users);
+  } else {
+    users.forEach((user) => {
+      if (user.id === parseInt(req.params.id)) return res.status(200).json(user);
+    });
+  }
+  return res.status(400).json({ status: "error", message: "Error!" });
 });
 
 router.post("/", (req, res) => {
   const body = req.body;
-  body.id = id;
 
-  if (!body.name || !body.last_name || !body.age || !body.email) {
-    return res.status(400).json({ status: "error", message: "Error!" });
+  if (!isArray(body)) {
+    body.id = id;
+    if (!body.name || !body.last_name || !body.age || !body.email) {
+      return res.status(400).json({ status: "error", message: "Error!" });
+    }
+    users.push(body);
+    id++;
+    return res.status(201).json({
+      status: "success",
+      message: `User ${body.id} created!`,
+      user: body,
+    });
   }
-
-  users.push(body);
-  id++;
-
-  return res.status(201).json({
-    status: "success",
-    message: `User ${body.id} created!`,
-    user: body,
-  });
 });
 
 router.put("/", (req, res) => {
@@ -56,12 +68,14 @@ router.delete("/", (req, res) => {
 
   if (body.id) {
     console.log(body.id);
-    users = users.filter((user) => user.id !== body.id);
-    return res.status(201).json({
-      status: "success",
-      message: `User ${body.id} deleted!`,
-      user: body,
-    });
+    if (users !== users.filter((user) => user.id !== body.id)) {
+      users = users.filter((user) => user.id !== body.id);
+      return res.status(201).json({
+        status: "success",
+        message: `User ${body.id} deleted!`,
+        user: body,
+      });
+    }
   }
 
   return res.status(400).json({ status: "error", message: "Error!" });
