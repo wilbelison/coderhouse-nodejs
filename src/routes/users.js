deb47227bc05df728ae2b914b2c8d7ba586117ca
Wilbelison Junior <meustudio@gmail.com>
2024-09-27 22:13:32 -0300

    crud com array no js

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-const users = [];
+let users = [];
+let id = 1;
 
 router.get("/", (req, res) => {
   res.status(200).json(users);
@@ -10,19 +11,60 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const body = req.body;
+  body.id = id;
 
-  if (!body.name || !body.email) {
-    return res
-      .status(400)
-      .json({ status: "error", message: "Name and email are required." });
+  if (!body.name || !body.last_name || !body.age || !body.email) {
+    return res.status(400).json({ status: "error", message: "Error!" });
   }
 
   users.push(body);
+  id++;
 
   return res.status(201).json({
     status: "success",
-    message: `${body.name} and ${body.email} are created!`,
+    message: `User ${body.id} created!`,
+    user: body,
   });
+});
+
+router.put("/", (req, res) => {
+  const body = req.body;
+
+  if (!body.id) {
+    return res.status(400).json({ status: "error", message: "Error!" });
+  }
+
+  users.forEach((user) => {
+    if (user.id === body.id) {
+      console.log(user.id);
+      for (const [key, value] of Object.entries(body)) {
+        user[key] = value;
+      }
+      return res.status(201).json({
+        status: "success",
+        message: `User ${body.id} updated!`,
+        user,
+      });
+    }
+  });
+
+  return res.status(400).json({ status: "error", message: "Error!" });
+});
+
+router.delete("/", (req, res) => {
+  const body = req.body;
+
+  if (body.id) {
+    console.log(body.id);
+    users = users.filter((user) => user.id !== body.id);
+    return res.status(201).json({
+      status: "success",
+      message: `User ${body.id} deleted!`,
+      user: body,
+    });
+  }
+
+  return res.status(400).json({ status: "error", message: "Error!" });
 });
 
 export default router;
